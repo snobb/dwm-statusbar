5842a98ae8658f33ba1b6ceb7073b625b9cca49d
akozadaev <akozadaev@yahoo.com>
2012-01-14 12:39:12 +0000

    Giving variables sensible names + adding X11 code

--- statusbar.c
+++ statusbar.c
@@ -4,9 +4,11 @@
 #include "build_host.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include <time.h>
+#include <signal.h>
 #include <X11/Xlib.h>
 
 
@@ -14,56 +16,75 @@
 
 #define LABUF     15
 #define DTBUF     20
-#define FULLSTR   52
+#define STR       60
 
 void xerror(const char *msg, ...);
 void set_status(char *str);
+void open_display(void);
+void close_display();
 void get_load_avg(char *buf);
 float get_battery(void);
 void get_datetime(char *buf);
 int get_wifi(void);
 int read_int(const char *path);
-void read_str(const char *path, char *buf, size_t sz); 
+void read_str(const char *path, char *buf, size_t sz);
 
 static Display *dpy;
 
 int
-main(void) {
+main(void)
+{
   float bat;                 /* battery status */
-  unsigned int lnk;          /* wifi link      */
+  int lnk;                   /* wifi link      */
   char la[LABUF] = "\0";     /* load average   */
   char dt[DTBUF] = "\0";     /* date/time      */
-  char full[FULLSTR] = "\0"; /* full string    */
+  char stat[STR] = "\0";     /* full string    */
 
+  open_display();
 
-  if (!(dpy = XOpenDisplay(NULL))) {
-    xerror("Cannot open display.\n");
-    return 1;
-  }
-
-  for (;;sleep(1)) {
+  while (!sleep(1)) {
     get_load_avg(la);
     lnk = get_wifi();
     bat = get_battery();
     get_datetime(dt);
 
-    snprintf(full, FULLSTR, "%s | %d | %0.1f%% | %s", la, lnk, bat, dt);
-    set_status(full);
+    snprintf(stat, STR, "%s | %d | %0.1f%% | %s", la, lnk, bat, dt);
+    set_status(stat);
   }
-  
-  XCloseDisplay(dpy);
-  
+
+  close_display();
   return 0; 
 }
 
 void
-set_status(char *str) {
+set_status(char *str)
+{
   XStoreName(dpy, DefaultRootWindow(dpy), str);
   XSync(dpy, False);
 }
 
 void
-xerror(const char *msg, ...) {
+open_display(void)
+{
+  if (!(dpy = XOpenDisplay(NULL))) {
+    xerror("Cannot open display.\n");
+    exit(1);
+  }
+  signal(SIGINT, close_display);
+  signal(SIGTERM, close_display);
+}
+
+void
+close_display()
+{
+  XCloseDisplay(dpy);
+  fputs("statusbar: exiting...\n", stderr);
+  exit(0);
+}
+
+void
+xerror(const char *msg, ...)
+{
   va_list ap;
 
   fprintf(stderr, "%s", ERR_PREFIX); 
@@ -74,12 +95,14 @@ xerror(const char *msg, ...) {
 }
 
 void
-get_load_avg(char *buf) {
+get_load_avg(char *buf)
+{
   read_str(LA_PATH, buf, LABUF);
 }
 
 float
-get_battery(void) {
+get_battery(void)
+{
   int now, full;
 
   now = read_int(BAT_NOW);
@@ -89,22 +112,24 @@ get_battery(void) {
 }
 
 void
-get_datetime(char *buf) {
+get_datetime(char *buf)
+{
   time_t rawtime;
 
   time(&rawtime);
   snprintf(buf, DTBUF, "%s", ctime(&rawtime));
-  buf[DTBUF-1] = '\0';
 }
 
 int
-get_wifi(void) {
+get_wifi(void)
+{
   int wifi = read_int(LNK_PATH);
   return wifi;
 }
 
 int
-read_int(const char *path) {
+read_int(const char *path)
+{
   FILE *fh;
   int i = 0;
 
@@ -113,33 +138,31 @@ read_int(const char *path) {
     xerror("Cannot open %s for reading.\n", path);
     return -1;
   }
+  
   if (fscanf(fh, "%d", &i) < 0) {
     xerror("Cannot read from %s\n", path);
   }
-
+  
   fclose(fh);
   return i;
 }
 
 void
-read_str(const char *path, char *buf, size_t sz) {
+read_str(const char *path, char *buf, size_t sz)
+{
   FILE *fh;
-  size_t rcv;
 
   fh = fopen(path, "r");
   if (fh == NULL) {
     xerror("Cannot open %s for reading.\n", path);
     return;
   }
-
-  rcv = fread(buf, 1, sz, fh);
-  if (ferror(fh) != 0) {
+  
+  if (fgets(buf, sz, fh) == NULL) {
     xerror("Cannot read from %s.\n", path);
-    clearerr(fh);
   }
+  
   fclose(fh);
-
-  buf[rcv-1] = '\0';
 }
 
 /*  EOF  */
